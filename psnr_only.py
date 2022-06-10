06209ad71050f64b153948b4bea1e45355f25ea8
XIAOMING ZHANG <49831490+OwEnXiaoMing@users.noreply.github.com>
2022-06-10 14:37:06 +0800

    Update psnr_only.py

--- psnr_only.py
+++ psnr_only.py
@@ -8,34 +8,20 @@ import torch
 
 from utils import util_calculate_psnr_ssim as util
 
-'--task classical_sr --scale 2 --training_patch_size 64 --model_path model_zoo/swinir/001_classicalSR_DF2K_s64w8_SwinIR-M_x2.pth --folder_lq testsets/Set5/LR_bicubic/X2 --folder_gt  testsets/Set5/HR'
-
-
+#--task classical_sr --scale 4 --folder_gt  testsets/div2k/HR --folder_sr results/ntire_div2kx4
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--task', type=str, default='color_dn', help='classical_sr, lightweight_sr, real_sr, '
                                                                      'gray_dn, color_dn, jpeg_car')
     parser.add_argument('--scale', type=int, default=1, help='scale factor: 1, 2, 3, 4, 8')  # 1 for dn and jpeg car
-    parser.add_argument('--noise', type=int, default=15, help='noise level: 15, 25, 50')
-    parser.add_argument('--jpeg', type=int, default=40, help='scale factor: 10, 20, 30, 40')
-    parser.add_argument('--training_patch_size', type=int, default=128, help='patch size used in training SwinIR. '
-                                                                             'Just used to differentiate two different settings in Table 2 of the paper. '
-                                                                             'Images are NOT tested patch by patch.')
-    parser.add_argument('--large_model', action='store_true', help='use large model, only provided for real image sr')
-    parser.add_argument('--model_path', type=str,
-                        default='model_zoo/swinir/001_classicalSR_DIV2K_s48w8_SwinIR-M_x2.pth')
-    parser.add_argument('--folder_lq', type=str, default=None, help='input low-quality test image folder')
     parser.add_argument('--folder_gt', type=str, default=None, help='input ground-truth test image folder')
-    parser.add_argument('--tile', type=int, default=None,
-                        help='Tile size, None for no tile during testing (testing as a whole)')
-    parser.add_argument('--tile_overlap', type=int, default=32, help='Overlapping of different tiles')
+    parser.add_argument('--folder_sr', type=str, default=None, help='input sr image folder')
     args = parser.parse_args()
 
     # setup folder and path
-    folder, save_dir, border, window_size = setup(args)
+    folder, border = setup(args)
     # print(border)
     border = 2
-    os.makedirs(save_dir, exist_ok=True)
     test_results = OrderedDict()
     test_results['psnr'] = []
     test_results['ssim'] = []
@@ -43,19 +29,14 @@ def main():
     test_results['ssim_y'] = []
     test_results['psnr_b'] = []
     psnr, ssim, psnr_y, ssim_y, psnr_b = 0, 0, 0, 0, 0
-
+    print('folder',folder)
     for idx, path in enumerate(sorted(glob.glob(os.path.join(folder, '*')))):
         # read image
-        path_hr = 'results\\ntire_div2kx4\\' + path.replace('testsets/div2k/HR', '')
 
-        path_lr = 'testsets\div2k\HR' + path.replace('testsets/div2k/HR', '')
+        path_hr = args.folder_sr + path.replace(args.folder_gt, '')
 
-        imgname, img_gt, img_ht, img_lq = get_image_pair(args, path, path_hr, path_lr)  # image to HWC-BGR, float32
-        img_lq = np.transpose(img_lq if img_lq.shape[2] == 1 else img_lq[:, :, [2, 1, 0]],
-                              (2, 0, 1))  # HCW-BGR to CHW-RGB
-        img_lq = torch.from_numpy(img_lq).float().unsqueeze(0).to('cpu')  # CHW-RGB to NCHW-RGB
+        imgname, img_gt, img_ht = get_image_pair(args, path, path_hr)  # image to HWC-BGR, float32
 
-        _, _, h_old, w_old = img_lq.size()
 
         # save image
         # img_ht=torch.as_tensor(img_ht)
@@ -65,7 +46,7 @@ def main():
         # evaluate psnr/ssim/psnr_b
         if img_gt is not None:
             img_gt = (img_gt * 255.0).round().astype(np.uint8)  # float32 to uint8
-            img_gt = img_gt[:h_old * args.scale, :w_old * args.scale, ...]  # crop gt
+
             img_gt = np.squeeze(img_gt)
 
             psnr = util.calculate_psnr(output, img_gt, crop_border=border)
@@ -104,25 +85,25 @@ def main():
 def setup(args):
     # 001 classical image sr/ 002 lightweight image sr
     if args.task in ['classical_sr', 'lightweight_sr']:
-        save_dir = f'results/swinir_{args.task}_x{args.scale}'
         folder = args.folder_gt
         border = args.scale
-        window_size = 8
-
-    return folder, save_dir, border, window_size
 
 
-def get_image_pair(args, path, path_hr, path_lr):
+    return folder, border
+
+
+def get_image_pair(args, path, path_hr):
     (imgname, imgext) = os.path.splitext(os.path.basename(path))
 
     # 001 classical image sr/ 002 lightweight image sr (load lq-gt image pairs)
     if args.task in ['classical_sr', 'lightweight_sr']:
         img_gt = cv2.imread(path, cv2.IMREAD_COLOR).astype(np.float32) / 255.
         img_hr = cv2.imread(path_hr, cv2.IMREAD_COLOR).astype(np.float32) / 255.
-        img_lq = cv2.imread(path_lr, cv2.IMREAD_COLOR).astype(np.float32) / 255.
+        #img_lq = cv2.imread(path_lr, cv2.IMREAD_COLOR).astype(np.float32) / 255.
 
-    return imgname, img_hr, img_gt, img_lq
+    return imgname, img_hr, img_gt
 
 
 if __name__ == '__main__':
     main()
+
